47cbc301744d2d5474b79820f2df7803286748f0
Shakk77777 <shakk7777@gmail.com>
2023-05-22 18:08:31 -0500

    first commit

---
+++ operations.js
@@ -0,0 +1,61 @@
+// task 1
+var Superhero
+Superhero = "Shakman"
+var score 
+score = 5
+var level
+level = 0 < score && score < 10
+console.log(Superhero + " has mid-level skills: " + level)
+
+// task 2
+var damageReceived
+damageReceived = 0
+var battleTimeLeft
+battleTimeLeft = 0
+var energy 
+energy = 10
+var level_2
+level_2 = battleTimeLeft == 0 || energy > damageReceived
+
+console.log(" Game Over: " + level_2)
+
+
+// task 3
+var num1
+num1 = 2
+var num2
+num2 = 5
+var test1
+test1 = num1 % 2
+test2 = num2 % 2
+var result1
+result1 = test1 == 0
+var result2
+result2 = test2 == 0
+console.log(" Is " +  num1 + ' an even number? ' + result1)
+console.log(" Is " +  num2 + " an even number? " + result2)
+
+
+// task 4
+
+console.log(100 + 10)
+
+
+// task 5
+var me 
+me = " I know "
+var three
+three = 3
+var proLang
+proLang = " programming languages "
+console.log(me + three + proLang)
+
+
+// task 6
+
+var mySkills
+mySkills = 0
+mySkills += 5
+mySkills += 15
+console.log(mySkills)
+
